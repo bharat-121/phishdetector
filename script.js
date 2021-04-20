e48e3779182eea6b656d5cbfdf3485813694bbe1
Bharat <branjan121@gmail.com>
2021-04-20 22:59:54 +0530

    latest code running alert on each web page

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const REQUEST_URL = "http://127.0.0.1:8000/model/";
-var url="";
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    url=tabs[0].url;
-});
 
 $(document).ready(function() {
-  $("#check-phishing").click(function() {
+    console.log("heelo");
+    const url = window.location.href;
     console.log(url);
+    
     const query = REQUEST_URL + "?url=" + url;
     $.get(query, function(data, status) {
         if(data.response=="1")
         {
+            
             console.log("LEG");
-            $("#site_msg").text("Safe to Use");
+            alert("LEG");
         }
         else{
-            
             console.log("PHISH");
-            $("#site_msg").text("UnSafe to Use");
-
+            alert("PHISH");
         }
     });
-  });
 });
